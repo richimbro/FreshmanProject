d23a65194cbfb53ae75315961dc83859d545beab
Rich Imbro <rimbro1@binghamton.edu>
2019-11-11 16:42:00 -0500

    hero

---
+++ Hero.py
@@ -0,0 +1,9 @@
+Class Hero:
+
+    def __init__(self, image, height, width, speed, health,):
+        self.image = image
+        self.height = height
+        self.speed = speed
+        self.width= width
+        self.health = health
+        se
