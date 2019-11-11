64ccb5e4a1b61fa7c291cd038cacd75cd01ceddf
Rich Imbro <rimbro1@binghamton.edu>
2019-11-11 16:54:19 -0500

    monday

--- Hero.py
+++ Hero.py
@@ -1,9 +1,28 @@
-Class Hero:
+import pygame
+import random
 
-    def __init__(self, image, height, width, speed, health,):
-        self.image = image
+class Hero:
+
+    def __init__(self, image, height, width, speed, health, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load()
+        self.rect = self.image.get_rect()
         self.height = height
         self.speed = speed
         self.width= width
         self.health = health
-        se
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 3
+        self.health = 1 #How many lives do we want him having?
+
+    def move_left(self):
+        self.rect.x -= self.speed
+    def move_right(self):
+        self.rect.x += self.speed
+    def jump(self): #going to have to fix this
+        self.rect.y += self.speed
+        self.rect.y -= self.speed
+
+    def fight(self, enemy):
+        #are we  having  an enemy?
