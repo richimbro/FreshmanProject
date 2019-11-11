de9bafb9ef9920a71a90b26b3c945396932f8d2c
Rich Imbro <rimbro1@binghamton.edu>
2019-11-11 16:42:36 -0500

    next

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import pygame
-
-def main():
-    pygame.init()
-=======
 #import your controller
 import pygame
 
 def main():
 	pygame.init()
->>>>>>> 793a5eea6283ba447d0d646737d3175bcc3f9c49
     #Create an instance on your controller object
 main()#import your controller
