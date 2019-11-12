edfb7f0e8214eaa2614f275e21730876d0a95511
Anthony Goncalves <agoncal2@binghamton.edu>
2019-11-12 15:11:04 -0500

    tabs

--- files/Enemy.py
+++ files/Enemy.py
@@ -4,21 +4,21 @@ import os
 
 class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self, x , y, image, height, width):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load()
-        self.rect = self.image.get_rect()
-        self.height = height
-        self.speed = speed
-        self.width= width
-        self.health = health
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = 3
-        self.health = 1
+	def __init__(self, x , y, image, height, width):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load()
+		self.rect = self.image.get_rect()
+		self.height = height
+		self.speed = speed
+		self.width= width
+		self.health = health
+		self.rect.x = x
+		self.rect.y = y
+		self.speed = 3
+		self.health = 1
 
-    def shoot(self):
-		
+	def shoot(self):
+
 
 	def jump(self):
 		while True:
