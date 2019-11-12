0310be20b54d0f1c582f924a638684eb2da23bc0
Rich Imbro <rimbro1@binghamton.edu>
2019-11-12 14:34:05 -0500

    again

--- files/Platform.py
+++ files/Platform.py
@@ -1,3 +1,14 @@
+import pygame
+import sys
+import os
+
 class Platform:
 
-    def __init__(self,length, width, image, )
+    def __init__(self, x, y, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images',image)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
