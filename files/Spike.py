aa31a1e8a3afd97193bd46ad101381e9741ed3a0
Rich Imbro <rimbro1@binghamton.edu>
2019-11-12 14:18:24 -0500

    spike initial

---
+++ files/Spike.py
@@ -0,0 +1,11 @@
+class Spike:
+
+    def __init__(self, image, x, y, height, width)
+        self.image =
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
+    def collision():
+        if 
